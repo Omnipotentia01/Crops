1d4b6e97d4efc0528d55d953308b4e152459ed17
Tommy Cakebread <tommy_tlc@hotmail.co.uk>
2014-06-23 00:32:09 +0100

    Sheep + Animal
    Added Animal_class and sheep subclass
    + Spelling errors. (Potata to Potato)

--- sheep_class.py
+++ sheep_class.py
@@ -1,17 +1,17 @@
 from cropclass import *
 
-class Potato(Crop):
-    """ A representaion of a potato crop"""
+class Sheep(Crop):
+    """ A representaion of a sheep crop"""
     #Constructor
     def __init__(self):
         #Call super constructor
         super().__init__(1,3,6)
-        self._type = "Potato"
+        self._type = "Sheep"
 
     #Override previous methods for subclass (grow in this case)
-        def grow(self,light,water):
-            if light >= self._light_need and water >= self._water_need:
-                if self._status == "Seedling" and water > self._water_need:
+        def grow(self,food,water):
+            if food >= self._food_need and water >= self._water_need:
+                if self._status == "Newborn" and water > self._water_need:
                     self.growth += self._growth_rate * 1.5
                 elif self._status == "Young" and water > self.water_need:
                     self._growth += self._growth_rate *1.25
@@ -23,14 +23,14 @@ class Potato(Crop):
             self._update_status()
 
 def main():
-    #Create Potato crop
-    potato_crop = Potato()
-    print(potato_crop.report())
+    #Create Sheep crop
+    sheep_crop = Sheep()
+    print(sheep_crop.report())
     #manualy grow the crop
-    manual_grow(potato_crop)
-    print(potato_crop.report())
-    manual_grow(potato_crop)
-    print(potato_crop.report())            
+    manual_grow(sheep_crop)
+    print(sheep_crop.report())
+    manual_grow(sheep_crop)
+    print(sheep_crop.report())            
 
 if __name__ == "__main__":
     main()
